4fac6bc2a781cc3a6ec23976847f00c1e9c80145
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-12-15 21:27:45 -0300

    feat: add LoadingToast component for loading state indication

---
+++ client/src/app/countries/components/LoadingToast.tsx
@@ -0,0 +1,10 @@
+const LoadingToast = () => {
+  return (
+    <div className="toast toast-middle toast-center">
+      <div className="alert alert-info">
+        <span>Loading info...</span>
+      </div>
+    </div>
+  );
+};
+export default LoadingToast;
